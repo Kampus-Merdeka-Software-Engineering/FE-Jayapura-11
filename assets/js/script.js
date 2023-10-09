1dd2f51f05a26b976f050b77cd32f78d6889da37
Jovan <jovanaan12@gmail.com>
2023-10-09 22:57:31 +0700

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,24 +59,3 @@ var swiper = new Swiper(".ourteam-slider", {
     },
   },
 });
-
-// var swiper = new Swiper(".reviews-slider", {
-//   spaceBetween: 20,
-//   grabCursor: true,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     540: {
-//       slidesPerView: 1,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//     },
-//     1024: {
-//       slidesPerView: 3,
-//     },
-//   },
-// });
